6f0aa02f75822ea3b24a0d99eb028b890bbde156
Amilkhael <amilkhael@outlook.com>
2018-06-08 18:57:03 -0500

    Página en cnstrucción de Idivisa

--- header.php
+++ header.php
@@ -11,13 +11,13 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="author" content="La Salle">
-	<meta name="description" content="Documentos de uso para la imagen institucional">
+	<meta name="description" content="La Salle font family">
 
 
 	<!-- Mobile Metas -->
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
 	<!-- Titulo -->
-	<title>Título | La Salle</title>
+	<title>Indivisa | La Salle</title>
 
 	<!--css-->
 	<link rel="stylesheet" href="css/normalize.css">
@@ -36,7 +36,7 @@
 	
 	<!-- favicon -->
 	<link rel="icon" type="image/x-icon" href="images/favicon/favicon.ico">
-	<link rel="alternate" href="http://indivisafont.org/" hreflang="es-mx" />
+	<link rel="alternate" href="http://indivisafont.org/" hreflang="en" />
 
 </head>
 <body>
--- index.php
+++ index.php
@@ -5,7 +5,32 @@ include 'header.php';
 *	Documento: Index del micrositio
 */
 ?>
+<section class="container-fluid">
+	
+		<div class="row bgAzul bgTextura align-items-end">
+			<div class="col">
+				<h1>Indivisa</h1>
+			</div>
+			
+		</div>
+	
+	<div class="row">
+		<div class="col-12">
+				<h2>La Salle</h2>
+				<p class="titulo"><span class="cifra">270</span> supported languages</p>
+				<p class="contenido">of the Latin alphabet</p>
+				<p class="titulo"><span class="cifra">25,000</span> glyphs</p>
+				<p class="contenido">Capital letters, small caps, upper, lower, ligatures,<br>
+				mathematical, monetary, diacritic, punctuation, editorial, etc.</p>
+		</div>
+		<div class="col-12 contenidoFecha">
+				<p class="fecha"><span class="lineaVertical"></span>September 2018</p>
+				<p class="frase">Let this be the permanent expression of who we are and who we aspire to be.</p>
+				<p class="indivisaManent"> Indivisa Manent</p>
+		</div>
+	</div>
 
+</section>
 
 
 <?php include 'footer.php';?>
