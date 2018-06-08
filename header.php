8c3b2c170376df17edee2a36ed50e6ce5955f41f
Amilkhael <amilkhael@outlook.com>
2018-06-08 10:21:25 -0500

    Inicio del sitio
    Se tomó el template

---
+++ header.php
@@ -0,0 +1,53 @@
+<?php
+/**
+* 	@author Amilkhael Chávez Delgado;
+*	Documento: Header para las páginas
+*/
+?>
+<!DOCTYPE html>
+<html lang="es-419">
+<head>
+	<!--metas-->
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="author" content="La Salle">
+	<meta name="description" content="Documentos de uso para la imagen institucional">
+
+
+	<!-- Mobile Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
+	<!-- Titulo -->
+	<title>Título | La Salle</title>
+
+	<!--css-->
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/custom.css">
+	<!--[if IE]>
+			<link rel="stylesheet" href="css/ie.css">
+		<![endif]-->
+
+	<!--[if lte IE 8]>
+			<script src="js/respond/respond.js"></script>
+			<script src="js/excanvas/excanvas.js"></script>
+	<![endif]-->
+	<!--fonts-->
+	<!--js-->
+	
+	<!-- favicon -->
+	<link rel="icon" type="image/x-icon" href="images/favicon/favicon.ico">
+	<link rel="alternate" href="http://indivisafont.org/" hreflang="es-mx" />
+
+</head>
+<body>
+	
+    <!-- HEADER-->
+    <header class="container-fluid logo"> 
+      <div class="row">
+				<div class="col-12">
+				<a href="http://lasalle.mx"><img src="images/header/imagotipo/logo.png" alt="La Salle" class="img-responsive"></a>
+				</div>
+      </div>
+    </header>
+
+    <!--  FIN HEADER -->
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+include 'header.php';
+/**
+* 	@author Amilkhael Chávez Delgado;
+*	Documento: Index del micrositio
+*/
+?>
+
+
+
+<?php include 'footer.php';?>
