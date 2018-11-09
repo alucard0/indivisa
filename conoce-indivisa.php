2d34bacaa69f7d91ccfd00a737c6ee5c4b90be9e
Amilkhael <amilkhael@outlook.com>
2018-11-09 12:20:45 -0600

    Actualización
    Se agregarón las secciónes de familias y extensiones, rasgos distintivos de conoce indivisa

--- conoce-indivisa.php
+++ conoce-indivisa.php
@@ -90,6 +90,11 @@ include 'header.php';
 					<p>Formal y Juvenil</p>
 				</div>
 			</div>
+			<div class="row backgroundGrisClaro2 justify-content-center">
+				<div class="col-12 col-sm-12 col-md-11 col-lg-11 col-xl-11">
+					<img src="images/contenido/conoceIndivisa/G-ESP.png" alt="" class="img-fluid imagenIdioma" key="conoceIndivisa-g">
+				</div>
+			</div>
 		</div>
 		<div class="col-12 col col-sm-12 col-md-6 col-lg-6 col-xl-6">
 			<div class="row justify-content-center backgroundAmarillo filaProceso">
@@ -161,7 +166,7 @@ include 'header.php';
 					<div class="row justify-content-center align-items-end">
 						<div class="col-6 col-sm-6 col-md-5 col-lg-5 col-xl-5 letrasN TextSerif textoAzul">
 							<div class="row align-items-end justify-content-end">
-								<div class="col-7 col-sm-12 col-md-5 col-lg-5 col-xl-7  colSerif">
+								<div class="col-7 col-sm-12 col-md-12 col-lg-5 col-xl-7  colSerif">
 									<p><span class="light">n</span><span class="light-italic">n</span><br><span class="black">n</span><span class="black-italic">n</span></p>
 									<p class="familia">Serif</p>
 								</div>
@@ -169,7 +174,7 @@ include 'header.php';
 						</div>
 						<div class="col-6 col-sm-6 col-md-5 col-lg-5 col-xl-5 letrasN TextSans textoAzul">
 							<div class="row align-items-end justify-content-start">
-								<div class="col-9 col-sm-12 col-md-5 col-lg-5 col-xl-5  colSans">
+								<div class="col-9 col-sm-12 col-md-12 col-lg-5 col-xl-5  colSans">
 									<p><span class="light">n</span><span class="light-italic">n</span><br><span class="black">n</span><span class="black-italic">n</span></p>
 									<p class="familia">Sans</p>
 								</div>
@@ -178,6 +183,174 @@ include 'header.php';
 					</div>
 				</div>
 			</div>
+		</div>
+	</div>
+</section>
+
+<section id="familiasyExtensiones" class="container-fluid">
+	<div class="row justify-content-center">
+		<div class="col-12 col-sm-12 col-md-9 col-lg-9 col-xl-9">
+			<p class="titulo lang"  key="sistemaTipografico-titulo">Familias y extensiones</p>
+			<hr class="lineaAmarilla v2">
+		</div>
+		<div class="col-12 col-sm-12 col-md-9 col-lg-9 col-xl-9">
+			<p class="text-center subtitulo textoAzul">Diseñada para cumplir con los requerimientos de comunicación escrita de la comunidad lasallista.</p>
+			<p class="text-center subtitulo-p1">Funcional y versátil, es legible en textos con puntajes pequeños y potente en textos grandes.</p>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
+			<div class="card text">
+				<img class="card-img-top" src="images/contenido/conoceIndivisa/TEXT-SANS.jpg" alt="Card image cap">
+				<div class="card-body sans backgroundGrisClaro">
+					<p class="card-title backgroundAmarillo text-center nombreVarianteTipo regular-italic">Text Sans</p>
+					<div class="container-fluid ">
+						<div class="row justify-content-center no-gutters">
+							<div class="col-5">
+								<p class="nombreVarianteTipo light">Light</p>
+								<p class="nombreVarianteTipo regular">Regular</p>
+								<p class="nombreVarianteTipo bold">Bold</p>
+								<p class="nombreVarianteTipo black">Black</p>
+							</div>
+							<div class="col-6">
+								<p class="nombreVarianteTipo light-italic">Light Italic</p>
+								<p class="nombreVarianteTipo regular-italic">Regular Italic</p>
+								<p class="nombreVarianteTipo bold-italic">Bold Italic</p>
+								<p class="nombreVarianteTipo black-italic">Black Italic</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
+			<div class="card text">
+				<img class="card-img-top" src="images/contenido/conoceIndivisa/TEXT-SERIF.jpg" alt="Card image cap">
+				<div class="card-body serif backgroundGrisClaro">
+					<p class="card-title backgroundAmarillo text-center nombreVarianteTipo regular-italic">Text Serif</p>
+					<div class="container-fluid ">
+						<div class="row justify-content-center no-gutters">
+							<div class="col-5">
+								<p class="nombreVarianteTipo light">Light</p>
+								<p class="nombreVarianteTipo regular">Regular</p>
+								<p class="nombreVarianteTipo bold">Bold</p>
+								<p class="nombreVarianteTipo black">Black</p>
+							</div>
+							<div class="col-6">
+								<p class="nombreVarianteTipo light-italic">Light Italic</p>
+								<p class="nombreVarianteTipo regular-italic">Regular Italic</p>
+								<p class="nombreVarianteTipo bold-italic">Bold Italic</p>
+								<p class="nombreVarianteTipo black-italic">Black Italic</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
+			<div class="card display">
+				<img class="card-img-top" src="images/contenido/conoceIndivisa/DISPLAY-SANS.jpg" alt="Card image cap">
+				<div class="card-body sans backgroundGrisClaro">
+					<p class="card-title backGroundAzul text-center nombreVarianteTipo regular-italic textoBlanco">Display Sans</p>
+					<div class="container-fluid ">
+						<div class="row justify-content-center no-gutters">
+							<div class="col-5">
+								<p class="nombreVarianteTipo thin">Thin</p>
+								<p class="nombreVarianteTipo regular">Regular</p>
+								<p class="nombreVarianteTipo heavy">Heavy</p>
+							</div>
+							<div class="col-6">
+								<p class="nombreVarianteTipo thin-italic">Thin Italic</p>
+								<p class="nombreVarianteTipo regular-italic">Regular Italic</p>
+								<p class="nombreVarianteTipo heavy-italic">Heavy Italic</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
+			<div class="card display">
+				<img class="card-img-top" src="images/contenido/conoceIndivisa/DISPLAY-SERIF.jpg" alt="Card image cap">
+				<div class="card-body serif backgroundGrisClaro">
+					<p class="card-title backGroundAzul text-center nombreVarianteTipo regular-italic textoBlanco">Display Serif</p>
+					<div class="container-fluid ">
+						<div class="row justify-content-center no-gutters">
+							<div class="col-5 ">
+								<p class="nombreVarianteTipo thin">Thin</p>
+								<p class="nombreVarianteTipo regular">Regular</p>
+								<p class="nombreVarianteTipo heavy">Heavy</p>
+							</div>
+							<div class="col-6">
+								<p class="nombreVarianteTipo thin-italic">Thin Italic</p>
+								<p class="nombreVarianteTipo regular-italic">Regular Italic</p>
+								<p class="nombreVarianteTipo heavy-italic">Heavy Italic</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section id="rasgosDistintivos" class="container-fluid">
+	<div class="row justify-content-center">
+		<div class="col-12 col-sm-12 col-md-9 col-lg-9 col-xl-9">
+			<p class="titulo lang"  key="sistemaTipografico-titulo">Rasgos distintivos</p>
+			<hr class="lineaAmarilla v2">
+		</div>
+	</div>
+	<div class="row justify-content-center imagenesRasgos">
+		<div class="col-12 col-sm-12 col-md-6 col-lg-5 col-xl-5"><img src="images/contenido/conoceIndivisa/1_RASGOS.png" alt="" class="img-fluid"></div>
+		<div class="col-12 col-sm-12 col-md-6 col-lg-5 col-xl-5"><img src="images/contenido/conoceIndivisa/2_RASGOS.png" alt="" class="img-fluid"></div>
+		<div class="col-12 col-sm-12 col-md-6 col-lg-5 col-xl-5"><img src="images/contenido/conoceIndivisa/3_RASGOS.png" alt="" class="img-fluid"></div>
+		<div class="col-12 col-sm-12 col-md-6 col-lg-5 col-xl-5"><img src="images/contenido/conoceIndivisa/4_RASGOS.png" alt="" class="img-fluid"></div>
+	</div>
+</section>
+
+<section id="multilinguistico" class="container-fluid">
+	<div class="row justify-content-center">
+		<div class="col-12 col-sm-12 col-md-9 col-lg-9 col-xl-9">
+			<p class="titulo lang"  key="sistemaTipografico-titulo">Multilingüístico</p>
+			<hr class="lineaAmarilla v2">
+		</div>
+	</div>
+	<div class="row backgroundMapa">
+		<div class="col-8-12 col-sm-12 col-md-9 col-lg-8 col-xl-8">
+			<p class="subtitulo textoBlanco">Un Sistema que representa el lasallismo a nivel global</p>
+		</div>
+	</div>
+	<div class="row backgroundAmarillo contenidoNumeros">
+		<div class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+			<p class="numeraria">270 idiomas<br><span>con el alfabeto latino</span></p>
+		</div>
+		<div class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+			<p class="numeraria">25,000 glifos</p>
+		</div>
+		<div class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+			<p class="numeraria">28 varientes<br><span>16 variantes de Indivisa Text</span><br><span>12 variantes Indivisa Display</span></p>
+		</div>
+	</div>
+	<div class="row backGroundAzul justify-content-center">
+		<div class="col-xl-4">
+			<div class="round-button mx-auto"><p class="head">a<br><span>Letras</span>
+			</p></div>
+			<div class="row">
+				<div class="col-6">
+					<p class="text-right">AG</p>
+				</div>
+				<div class="col-6">
+					<p class="descripcion">Mayúsculas</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-xl-4">
+			
+		</div>
+		<div class="col-xl-4">
+			
 		</div>
 	</div>
 </section>
